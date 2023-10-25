a392925280da86d59ce1fbe942968c6defe828e2
Lavg21 <laviniag336@gmail.com>
2023-10-25 21:32:00 +0300

    created mapping options

---
+++ energy-management-system-frontend/src/app/components/mapping-options/mapping-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapping-options',
+  templateUrl: './mapping-options.component.html',
+  styleUrls: ['./mapping-options.component.css']
+})
+export class MappingOptionsComponent {
+
+}
