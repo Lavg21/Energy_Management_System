949b309e050a7ccf9850b33197dea4a41998ec16
Lavg21 <laviniag336@gmail.com>
2023-11-12 16:58:08 +0200

    Initial configuration of monitoring project

---
+++ monitoring/src/test/java/com/ems/emsmonitoring/EnergyManagementSystemMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ems.emsmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyManagementSystemMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
